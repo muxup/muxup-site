d2ae417fed35c92b401dd9835b746d94c8d79d34
Alex Bradbury <asb@muxup.com>
2022-09-17 08:03:52 +0100

    [nfc] Contributors => contributors in copyright headers

--- utils/server_test.sh
+++ utils/server_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright Muxup Contributors.
+# Copyright Muxup contributors.
 # Distributed under the terms of the MIT license, see LICENSE for details.
 # SPDX-License-Identifier: MIT
 
