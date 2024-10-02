c4284621b799f026dc1c40ea5f2b4f55b5acc08e
Alex Bradbury <asb@muxup.com>
2024-10-02 13:37:37 +0100

    [js] Add simple JS so quadruple-clicking in a <code> block within a <pre> selects it all

--- fragments/article.js
+++ fragments/article.js
@@ -5,3 +5,20 @@ const e = document.querySelector('#logo-highlight');
 const logoHLInfo = annotate(e, e.parentNode, "#ffdac0");
 initAnnotations();
 addRedrawOnMouseover(e, logoHLInfo);
+document.querySelectorAll('pre:has(code)').forEach(preEl => {
+  let clickCount = 0, lastClickTime = 0, lastClickedElement = null;
+  preEl.addEventListener('click', (event) => {
+    const now = Date.now();
+    if (event.target !== lastClickedElement || now - lastClickTime >= 500) {
+      clickCount = 1;
+    } else {
+      clickCount++;
+    }
+    if (clickCount === 4) {
+      window.getSelection().selectAllChildren(preEl);
+      clickCount = 0;
+    }
+    lastClickedElement = event.target;
+    lastClickTime = now;
+  });
+});
