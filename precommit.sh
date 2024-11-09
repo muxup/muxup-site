cdc1c773aa13cee7c967bffa274fd4f31628aa0d
Alex Bradbury <asb@muxup.com>
2024-11-09 14:21:17 +0000

    [gen] Refactor and add 'build' and 'serve' top-level commands

--- precommit.sh
+++ precommit.sh
@@ -13,13 +13,13 @@ warn() {
 }
 
 printf "Checking types:\n"
-mypy --ignore-missing-imports --strict build.py || warn "Mypy failed"
+mypy --ignore-missing-imports --strict gen || warn "Mypy failed"
 printf "Checking import sorts:\n"
-ruff check --target-version py312 --select I --diff build.py || warn "Import sort check filed"
+ruff check --target-version py312 --select I --diff gen || warn "Import sort check filed"
 printf "Checking formatting:\n"
-ruff format --target-version py312 --diff build.py || warn "Reformatting needed"
+ruff format --target-version py312 --diff gen || warn "Reformatting needed"
 printf "Checking flake8 lints:\n"
-flake8 --max-line-length 88 --extend-ignore=E203,E302,E501,W291 build.py || warn "Flake8 found issues"
+flake8 --max-line-length 88 --extend-ignore=E203,E302,E501,W291 gen || warn "Flake8 found issues"
 if [ $HAD_WARN -ne 0 ]; then
   warn "Exiting with failures"
   exit 1
